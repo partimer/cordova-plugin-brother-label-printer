9408a3895c1c92afcd5a5134cd38d0edff3a0486
jmcshane <jmcshane@bmendez-laptop.mkld.global>
2019-06-10 14:31:05 -0400

    removed old files; changed directory for customPaper

--- src/android/BasePrint.java
+++ src/android/BasePrint.java
@@ -456,9 +456,9 @@ public abstract class BasePrint {
 //         }
 //works
 //         Log.d("getPreferencesRJ2150", ( Common.CUSTOM_PAPER_FOLDER + "bsr16act.bin") );
-//         mPrinterInfo.customPaper = Common.CUSTOM_PAPER_FOLDER + "bsr16act.bin";
+        mPrinterInfo.customPaper = Common.CUSTOM_PAPER_FOLDER + "RJ2150_51x26mm.bin";
 //         mPrinterInfo.customPaper = "file:///android_asset/bsr16act.bin";
-        mPrinterInfo.customPaper = "file:///storage/emulated/0/Android/data/com.littledsys.harps.mobile/files/bsr16act.bin";
+//         mPrinterInfo.customPaper = "file:///storage/emulated/0/Android/data/com.littledsys.harps.mobile/files/bsr16act.bin";
 
         Log.d("getPreferencesRJ2150 Common.CUSTOM_PAPER_FOLDER", Common.CUSTOM_PAPER_FOLDER );
 //         Log.d("getPreferencesRJ2150 Context.getFilesDir();", Context.getFilesDir() );
@@ -469,6 +469,10 @@ public abstract class BasePrint {
         Log.d("getPreferencesRJ2150 java.home", System.getProperty("java.home") );
         Log.d("getPreferencesRJ2150 user.home", System.getProperty("user.home") );
         Log.d("getPreferencesRJ2150 java.class.path", System.getProperty("java.class.path") );
+        
+        raw2file("RJ2150_51x26mm.bin", R.raw.rj2150_51x26mm);
+
+        
     }
     
     /**
